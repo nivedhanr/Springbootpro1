15909c5c916cc7abfc09b09b781038be6b6424e7
nivedhanr <nivedharenahan@gmail.com>
2022-12-06 09:00:03 +0530

    model created

---
+++ src/main/java/com/example/studentappnew_backend/Model/Students.java
@@ -0,0 +1,9 @@
+package com.example.studentappnew_backend.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Students")
+public class Students {
+}
